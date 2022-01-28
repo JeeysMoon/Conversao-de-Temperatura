0e58d8e44378a31cc1c3fee205bba803562d34c5
Jennifer Sousa <jennifergaldinodesousa@gmail.com>
2022-01-28 20:50:35 -0300

    2801

---
+++ src/ex04/pkg13/pkg08aula03/EX041308AULA03.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex04.pkg13.pkg08aula03;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EX041308AULA03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Escreva um programa em Java que converta temperaturas em Fahrenheit (F) para Celcius (C). Sabendo que a temperatura em Celcius é 22 e a fórmula para conversão é F = C * 1.8 *
+        
+        Scanner input = new Scanner(System.in);
+        
+        float c;
+        
+        System.out.println("Informe a temperatura em Celsius: ");
+        c = input.nextFloat();
+        
+        System.out.println(" A temperatura em Fahrenheit: " +((c*1.8)+32));
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
